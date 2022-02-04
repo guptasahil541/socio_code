98f2ca05d83eab0e48576e99282f8b71d39a5e23
Sahil Gupta <guptasahil541@gmail.com>
2022-02-04 13:19:40 +0530

    Added routes and comments

--- index.js
+++ index.js
@@ -11,11 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded());
 app.use(express.static('assets'));
 
-app.get('/', function(req, res){
-    return res.render('home', {
-        title: "Homepage"
-    });
-});
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
